7c34a0b4079f54d52f5c81422612e5567233e3e9
corgie <jeffm1598753@gmail.com>
2024-09-13 13:44:29 +0800

    update

--- skip_list/skip_list.cpp
+++ skip_list/skip_list.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <utility>
+#include <vector>
 using namespace std;
 
 int main() {
+    vector<int> i(5);
     cout << "hello world" << "\n";
     for (int i = 0; i < 5; i++) {
         cout << i << "\n";
--- skip_list/skip_list.java
+++ skip_list/skip_list.java
@@ -1,5 +1,6 @@
 public class skip_list {
     public static void main(String[] args) {
         System.out.println("hello word");
+        
     }
 }
